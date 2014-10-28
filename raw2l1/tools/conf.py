5a455064cffb7fa282e1358c7a0b100b88441590
madrouin <marc-antoine.drouin@lmd.polytechnique.fr>
2014-10-28 11:16:39 +0100

    Minors correction to respect PEP8

--- raw2l1/tools/conf.py
+++ raw2l1/tools/conf.py
@@ -6,17 +6,20 @@ from __future__ import print_function, division, absolute_import
 import ConfigParser
 import logging
 
+
 def add(conf, input_args, logger):
     """
     Allow to add parameters in conf section of conf object
     """
 
-    # Warning: for configuration file, we do not use the filename but the filehandler
+    # Warning: for configuration file, we do not use the filename but the
+    # filehandler
     #   to access filename use, conf_file.name
     for key, value in input_args.items():
         conf.set('conf', key, value)
 
     return conf
+
 
 def init(input_args, logger):
     """
@@ -37,7 +40,7 @@ def init(input_args, logger):
         logger.debug("raw2l1 configuration")
         for section in conf.sections():
             for key, value in conf.items(section):
-                logger.debug('['+section+'] ' + key + ' : ' + repr(value))
+                logger.debug('[' + section + '] ' + key + ' : ' + repr(value))
         logger.debug("end of configuration")
 
     return conf
