71633dda7017800a237993f3143fc037b0bd9db0
Marc-Antoine Drouin <marc-antoine.drouin@lmd.polytechnique.fr>
2016-01-06 15:20:44 +0100

    add reader and test to convert air temperature files from hatpro

---
+++ raw2l1/test/test_rpg_hatpro.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import subprocess
+import os
+
+MAIN_DIR = os.path.dirname(os.path.dirname(__file__)) + os.sep
+CONF_DIR = MAIN_DIR + 'conf' + os.sep
+TEST_DIR = MAIN_DIR + 'test' + os.sep
+TEST_IN_DIR = TEST_DIR + os.sep + 'input' + os.sep
+TEST_OUT_DIR = TEST_DIR + os.sep + 'output' + os.sep
+PRGM = "raw2l1.py"
+
+
+class TestHatProBL00L2(unittest.TestCase):
+
+    IN_DIR = TEST_IN_DIR + 'rpg_hatpro' + os.sep
+
+    def test_chm15k_toprof(self):
+
+        date = '20150427'
+        test_ifile = self.IN_DIR + 'hatpro_0a_z1Imwrad-TPB_v01_20150930_000020_1436.nc'
+        test_ofile = TEST_OUT_DIR + 'sups_sir_mwrBL00_l2_ta_v01_20150930000020.nc'
+        test_cfile = CONF_DIR + 'conf_rpg_hatpro_bl00-l2_toprof_netcdf4.ini'
+
+        resp = subprocess.check_call([
+            MAIN_DIR + PRGM,
+            date,
+            test_cfile,
+            test_ifile,
+            test_ofile,
+            '-log_level',
+            'debug'
+        ])
+
+        self.assertEqual(resp, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
