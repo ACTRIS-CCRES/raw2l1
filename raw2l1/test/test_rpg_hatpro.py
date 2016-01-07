14a04b7b83b62c921ffb2ce2c4a7aaedb147536c
Marc-Antoine Drouin <marc-antoine.drouin@lmd.polytechnique.fr>
2016-01-07 17:43:14 +0100

    modify test for hatpro readers

--- raw2l1/test/test_rpg_hatpro.py
+++ raw2l1/test/test_rpg_hatpro.py
@@ -13,11 +13,11 @@ TEST_OUT_DIR = TEST_DIR + os.sep + 'output' + os.sep
 PRGM = "raw2l1.py"
 
 
-class TestHatProL2BLAirTemp(unittest.TestCase):
+class TestRunHatPro(unittest.TestCase):
 
     IN_DIR = TEST_IN_DIR + 'rpg_hatpro' + os.sep
 
-    def test_chm15k_toprof(self):
+    def test_rpg_hatpro_bl_ta_toprof(self):
 
         date = '20150427'
         test_ifile = self.IN_DIR + 'hatpro_0a_z1Imwrad-TPB_v01_20150930_000020_1436.nc'
@@ -36,17 +36,31 @@ class TestHatProL2BLAirTemp(unittest.TestCase):
 
         self.assertEqual(resp, 0)
 
-
-class TestHatProL2AirTemp(unittest.TestCase):
-
-    IN_DIR = TEST_IN_DIR + 'rpg_hatpro' + os.sep
-
-    def test_chm15k_toprof(self):
+    def test_rpg_hatpro_ta_toprof(self):
 
         date = '20150427'
         test_ifile = self.IN_DIR + 'hatpro_0a_z1Imwrad-TPC_v01_20150930_000542_1433.nc'
         test_ofile = TEST_OUT_DIR + 'sups_sir_mwr00_l2_ta_v01_20150930000020.nc'
         test_cfile = CONF_DIR + 'conf_rpg_hatpro_l2-ta_toprof_netcdf4.ini'
+
+        resp = subprocess.check_call([
+            MAIN_DIR + PRGM,
+            date,
+            test_cfile,
+            test_ifile,
+            test_ofile,
+            '-log_level',
+            'debug'
+        ])
+
+        self.assertEqual(resp, 0)
+
+    def test_rpg_hatpro_hua_toprof(self):
+
+        date = '20150427'
+        test_ifile = self.IN_DIR + 'hatpro_0a_z1Imwrad-HPC_v01_20150930_000542_1433.nc'
+        test_ofile = TEST_OUT_DIR + 'sups_sir_mwr00_l2_hua_v01_20150930000020.nc'
+        test_cfile = CONF_DIR + 'conf_rpg_hatpro_l2-hua_toprof_netcdf4.ini'
 
         resp = subprocess.check_call([
             MAIN_DIR + PRGM,
