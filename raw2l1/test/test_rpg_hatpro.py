8c9b53a23ad1a9882791b3a6afc353d681d55715
Marc-Antoine Drouin <marc-antoine.drouin@lmd.polytechnique.fr>
2016-01-07 16:26:56 +0100

    add conf file for hatpro L2 air temp files and rename reader to be more precise

--- raw2l1/test/test_rpg_hatpro.py
+++ raw2l1/test/test_rpg_hatpro.py
@@ -13,7 +13,7 @@ TEST_OUT_DIR = TEST_DIR + os.sep + 'output' + os.sep
 PRGM = "raw2l1.py"
 
 
-class TestHatProBL00L2(unittest.TestCase):
+class TestHatProL2BLAirTemp(unittest.TestCase):
 
     IN_DIR = TEST_IN_DIR + 'rpg_hatpro' + os.sep
 
@@ -22,7 +22,31 @@ class TestHatProBL00L2(unittest.TestCase):
         date = '20150427'
         test_ifile = self.IN_DIR + 'hatpro_0a_z1Imwrad-TPB_v01_20150930_000020_1436.nc'
         test_ofile = TEST_OUT_DIR + 'sups_sir_mwrBL00_l2_ta_v01_20150930000020.nc'
-        test_cfile = CONF_DIR + 'conf_rpg_hatpro_bl00-l2_toprof_netcdf4.ini'
+        test_cfile = CONF_DIR + 'conf_rpg_hatpro_bl00-l2-ta_toprof_netcdf4.ini'
+
+        resp = subprocess.check_call([
+            MAIN_DIR + PRGM,
+            date,
+            test_cfile,
+            test_ifile,
+            test_ofile,
+            '-log_level',
+            'debug'
+        ])
+
+        self.assertEqual(resp, 0)
+
+
+class TestHatProL2AirTemp(unittest.TestCase):
+
+    IN_DIR = TEST_IN_DIR + 'rpg_hatpro' + os.sep
+
+    def test_chm15k_toprof(self):
+
+        date = '20150427'
+        test_ifile = self.IN_DIR + 'hatpro_0a_z1Imwrad-TPC_v01_20150930_000542_1433.nc'
+        test_ofile = TEST_OUT_DIR + 'sups_sir_mwr00_l2_ta_v01_20150930000020.nc'
+        test_cfile = CONF_DIR + 'conf_rpg_hatpro_l2-ta_toprof_netcdf4.ini'
 
         resp = subprocess.check_call([
             MAIN_DIR + PRGM,
