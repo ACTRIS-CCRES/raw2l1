bf116fcb16411e9b8d5c99332a50a3fe524a717c
Marc-Antoine Drouin <marc-antoine.drouin@lmd.polytechnique.fr>
2016-04-12 10:22:50 +0200

    correct bug in vaisala reader when the message type is changing in the file

--- raw2l1/test/test_vaisala.py
+++ raw2l1/test/test_vaisala.py
@@ -139,5 +139,32 @@ class TestVaisalaSwissAirport(unittest.TestCase):
         self.assertEqual(resp, 0, 'CL swiss airport')
 
 
+class TestVaisalaBugSIRTA(unittest.TestCase):
+
+    IN_DIR = TEST_IN_DIR + 'vaisala_cl31' + os.sep
+    conf_file = CONF_DIR + 'conf_vaisala_cl31_toprof_netcdf4.ini'
+
+    def test_20150911(self):
+
+        date = '20150911'
+        test_ifile = (
+            self.IN_DIR + 'cl31_0a_z1R5mF3s_v02_20150911_*.asc'
+        )
+        test_ofile = TEST_OUT_DIR + 'test_cl-sirta_20150911.nc'
+
+        resp = subprocess.check_call([
+            MAIN_DIR + PRGM,
+            date,
+            self.conf_file,
+            test_ifile,
+            test_ofile,
+            '-log_level',
+            'debug',
+            '-v',
+            'debug'
+        ])
+
+        self.assertEqual(resp, 0, 'CL SIRTA bug message type')
+
 if __name__ == '__main__':
     unittest.main()
