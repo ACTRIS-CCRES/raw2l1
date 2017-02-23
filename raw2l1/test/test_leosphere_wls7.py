b21e5a56a382398fbb0b60435ddb2ca9f1f5d425
Marc-Antoine Drouin <marc-antoine.drouin@lmd.polytechnique.fr>
2017-02-23 16:55:52 +0100

    add reader for leosphere WLS7 1s data - add tests for several version - example of configuration files need to be finished

--- raw2l1/test/test_leosphere_wls7.py
+++ raw2l1/test/test_leosphere_wls7.py
@@ -13,10 +13,250 @@ TEST_OUT_DIR = os.path.join(TEST_DIR, 'output')
 PRGM = "raw2l1.py"
 
 
-class TestRunWLS7(unittest.TestCase):
-    """test full run for leosphere WLS70 depending on input file version"""
+# class TestRunWLS710Min(unittest.TestCase):
+#     """test full run for leosphere WLS70 depending on input file version"""
 
-    IN_DIR = os.path.join(TEST_IN_DIR, 'wls7_10min')
+#     IN_DIR = os.path.join(TEST_IN_DIR, 'wls7_10min')
+
+#     def test_leosphere_wls7_102(self):
+#         """test file version V1.0.2"""
+
+#         date = '20110103'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.0.2-wlscerea_0a_windLz1M10mn-LR_v01_20110103_142000_590.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-10-v1.0.2.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+#     def test_leosphere_wls7_1022(self):
+#         """test file version V1.0.2.2"""
+
+#         date = '20110110'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.0.2.2-wlscerea_0a_windLz1M10mn-LR_v01_20110110_162000_470.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-10-v1.0.2.2.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+#     def test_leosphere_wls7_1024(self):
+#         """test file version V1.0.2.4"""
+
+#         date = '20110929'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.0.2.4-wlscerea_0a_windLz1M10mn-LR_v01_20110929_144000_570.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-111-v1.0.2.4.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+#     def test_leosphere_wls7_1112(self):
+#         """test file version V1.1.12"""
+
+#         date = '20150119'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.1.12-wlscerea_0a_windLz1M10mn-LR_v01_20150119_154000_510.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-111-v1.1.12.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+#     def test_leosphere_wls7_1113(self):
+#         """test file version V1.1.13"""
+
+#         date = '20150602'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.1.13-wlscerea_0a_windLz1M10mn-LR_v01_20150602_130000_670.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-111-v1.1.13.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+#     def test_leosphere_wls7_1113a(self):
+#         """test file version V1.1.13a"""
+
+#         date = '20120726'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.1.3a-wlscerea_0a_windLz1M10mn-LR_v01_20120726_001000_1440.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-111-v1.1.3.a.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+#     def test_leosphere_wls7_1115(self):
+#         """test file version V1.1.15"""
+
+#         date = '20160719'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.1.15-wlscerea_0a_windLz1M10mn-LR_v01_20160719_090000_910.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-111-v1.1.15.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+#     def test_leosphere_wls7_1116(self):
+#         """test file version V1.1.16"""
+
+#         date = '20130426'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.1.6-wlscerea_0a_windLz1M10mn-LR_v01_20130426_001000_1440.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-111-v1.1.6.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+#     def test_leosphere_wls7_119(self):
+#         """test file version V1.1.9"""
+
+#         date = '20140603'
+#         test_ifile = os.path.join(
+#             self.IN_DIR,
+#             'wls7v2-v1.1.9-wlscerea_0a_windLz1M10mn-LR_v01_20140603_001000_1440.txt')
+#         test_ofile = os.path.join(
+#             TEST_OUT_DIR,
+#             'Wls7-111-v1.1.9.nc')
+#         test_cfile = os.path.join(
+#             CONF_DIR,
+#             'conf_leosphere_wls7_10min.ini')
+
+#         resp = subprocess.call([
+#             os.path.join(MAIN_DIR, PRGM),
+#             date,
+#             test_cfile,
+#             test_ifile,
+#             test_ofile,
+#             '-v',
+#             'debug'
+#         ])
+
+#         self.assertEqual(resp, 0)
+
+
+class TestRunWLS71S(unittest.TestCase):
+    """test full run for leosphere WLS70 1s depending on input file version"""
+
+    IN_DIR = os.path.join(TEST_IN_DIR, 'wls7_1s')
 
     def test_leosphere_wls7_102(self):
         """test file version V1.0.2"""
@@ -24,13 +264,13 @@ class TestRunWLS7(unittest.TestCase):
         date = '20110103'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.0.2-wlscerea_0a_windLz1M10mn-LR_v01_20110103_142000_590.txt')
+            'wls7v2-v1.0.2-wlscerea_0a_windLz1R1s-LR_v01_20110103_000000_788.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls70-10-v1.0.2.nc')
+            'Wls7-1s-10-v1.0.2.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -47,16 +287,16 @@ class TestRunWLS7(unittest.TestCase):
     def test_leosphere_wls7_1022(self):
         """test file version V1.0.2.2"""
 
-        date = '20110901'
+        date = '20110110'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.0.2.2-wlscerea_0a_windLz1M10mn-LR_v01_20110110_162000_470.txt')
+            'wls7v2-v1.0.2.2-wlscerea_0a_windLz1R1s-LR_v01_20110110_000000_945.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls70-10-v1.1.4.nc')
+            'Wls7-1s-v1.0.2.2.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -73,16 +313,16 @@ class TestRunWLS7(unittest.TestCase):
     def test_leosphere_wls7_1024(self):
         """test file version V1.0.2.4"""
 
-        date = '20120101'
+        date = '20110929'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.0.2.4-wlscerea_0a_windLz1M10mn-LR_v01_20110929_144000_570.txt')
+            'wls7v2-v1.0.2.4-wlscerea_0a_windLz1R1s-LR_v01_20110929_000000_528.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls7-111-v1.0.2.4.nc')
+            'Wls7-1s-111-v1.0.2.4.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -102,13 +342,13 @@ class TestRunWLS7(unittest.TestCase):
         date = '20150119'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.1.12-wlscerea_0a_windLz1M10mn-LR_v01_20150119_154000_510.txt')
+            'wls7v2-v1.1.12-wlscerea_0a_windLz1R1s-LR_v01_20150119_000000_932.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls7-111-v1.1.12.nc')
+            'Wls7-1s-111-v1.1.12.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -125,16 +365,16 @@ class TestRunWLS7(unittest.TestCase):
     def test_leosphere_wls7_1113(self):
         """test file version V1.1.13"""
 
-        date = '20150701'
+        date = '20150602'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.1.13-wlscerea_0a_windLz1M10mn-LR_v01_20150602_130000_670.txt')
+            'wls7v2-v1.1.13-wlscerea_0a_windLz1R1s-LR_v01_20150602_125555_665.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls7-111-v1.1.13.nc')
+            'Wls7-1s-111-v1.1.13.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -151,16 +391,16 @@ class TestRunWLS7(unittest.TestCase):
     def test_leosphere_wls7_1113a(self):
         """test file version V1.1.13a"""
 
-        date = '20120704'
+        date = '20120726'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.1.3a-wlscerea_0a_windLz1M10mn-LR_v01_20120726_001000_1440.txt')
+            'wls7v2-v1.1.3a-wlscerea_0a_windLz1R1s-LR_v01_20120726_000000_1440.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls7-111-v1.1.3.a.nc')
+            'Wls7-1s-111-v1.1.3.a.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -177,16 +417,16 @@ class TestRunWLS7(unittest.TestCase):
     def test_leosphere_wls7_1115(self):
         """test file version V1.1.15"""
 
-        date = '20160913'
+        date = '20160719'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.1.15-wlscerea_0a_windLz1M10mn-LR_v01_20160719_090000_910.txt')
+            'wls7v2-v1.1.15-wlscerea_0a_windLz1R1s-LR_v01_20160719_000000_528.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls7-111-v1.1.15.nc')
+            'Wls7-1s-111-v1.1.15.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -206,13 +446,13 @@ class TestRunWLS7(unittest.TestCase):
         date = '20130426'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.1.6-wlscerea_0a_windLz1M10mn-LR_v01_20130426_001000_1440.txt')
+            'wls7v2-v1.1.6-wlscerea_0a_windLz1R1s-LR_v01_20130426_000000_1440.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls7-111-v1.1.6.nc')
+            'Wls7-1s-111-v1.1.6.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -226,19 +466,19 @@ class TestRunWLS7(unittest.TestCase):
 
         self.assertEqual(resp, 0)
 
-    def test_leosphere_wls7_1119(self):
-        """test file version V1.1.19"""
+    def test_leosphere_wls7_119(self):
+        """test file version V1.1.9"""
 
-        date = '20140605'
+        date = '20140603'
         test_ifile = os.path.join(
             self.IN_DIR,
-            'wls7v2-v1.1.9-wlscerea_0a_windLz1M10mn-LR_v01_20140603_001000_1440.txt')
+            'wls7v2-v1.1.9-wlscerea_0a_windLz1R1s-LR_v01_20140603_000000_1440.rtd')
         test_ofile = os.path.join(
             TEST_OUT_DIR,
-            'Wls7-111-v1.1.9.nc')
+            'Wls7-1s-111-v1.1.9.nc')
         test_cfile = os.path.join(
             CONF_DIR,
-            'conf_leosphere_wls7_10min.ini')
+            'conf_leosphere_wls7_1s.ini')
 
         resp = subprocess.call([
             os.path.join(MAIN_DIR, PRGM),
@@ -251,29 +491,3 @@ class TestRunWLS7(unittest.TestCase):
         ])
 
         self.assertEqual(resp, 0)
-
-    # def test_leosphere_wls7_2158(self):Timestamp (end of interval)
-    #     """test file version V2.1.58"""
-
-    #     date = '20100113'
-    #     test_ifile = os.path.join(
-    #         self.IN_DIR,
-    #         'Wls7-57-v2.1.58')
-    #     test_ofile = os.path.join(
-    #         TEST_OUT_DIR,
-    #         'Wls7-57-v2.1.58.nc')
-    #     test_cfile = os.path.join(
-    #         CONF_DIR,
-    #         'conf_leosphere_wls7.ini')
-
-    #     resp = subprocess.call([
-    #         os.path.join(MAIN_DIR, PRGM),
-    #         date,
-    #         test_cfile,
-    #         test_ifile,
-    #         test_ofile,
-    #         '-v',
-    #         'debug'
-    #     ])
-
-    #     self.assertEqual(resp, 0)
