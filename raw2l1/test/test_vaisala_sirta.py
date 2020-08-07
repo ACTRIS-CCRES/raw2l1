e059d643683ea9ea0b4820ff157c9c53f601dbba
Marc-Antoine Drouin <marc-antoine.drouin@lmd.polytechnique.fr>
2020-08-07 16:22:52 +0200

    correct bug with corrupted files from meteofrance (incomplete message in cl31 files)

--- raw2l1/test/test_vaisala_sirta.py
+++ raw2l1/test/test_vaisala_sirta.py
@@ -18,27 +18,27 @@ class TestVaisalaBugSIRTA(unittest.TestCase):
     IN_DIR = TEST_IN_DIR + "vaisala_cl31" + os.sep
     conf_file = CONF_DIR + "conf_vaisala_cl31_toprof_netcdf4.ini"
 
-    # def test_20150911(self):
+    def test_20150911(self):
 
-    #     date = '20150911'
-    #     test_ifile = (
-    #         self.IN_DIR + 'cl31_0a_z1R5mF3s_v02_20150911_*.asc'
-    #     )
-    #     test_ofile = TEST_OUT_DIR + 'test_cl-sirta_20150911.nc'
+        date = "20150911"
+        test_ifile = self.IN_DIR + "cl31_0a_z1R5mF3s_v02_20150911_*.asc"
+        test_ofile = TEST_OUT_DIR + "test_cl-sirta_20150911.nc"
 
-    #     resp = subprocess.check_call([
-    #         MAIN_DIR + PRGM,
-    #         date,
-    #         self.conf_file,
-    #         test_ifile,
-    #         test_ofile,
-    #         '-log_level',
-    #         'debug',
-    #         '-v',
-    #         'debug'
-    #     ])
+        resp = subprocess.check_call(
+            [
+                MAIN_DIR + PRGM,
+                date,
+                self.conf_file,
+                test_ifile,
+                test_ofile,
+                "-log_level",
+                "debug",
+                "-v",
+                "debug",
+            ]
+        )
 
-    #     self.assertEqual(resp, 0, 'CL SIRTA bug message type')
+        self.assertEqual(resp, 0, "CL SIRTA bug message type")
 
     def test_20150603(self):
 
@@ -61,6 +61,28 @@ class TestVaisalaBugSIRTA(unittest.TestCase):
         )
 
         self.assertEqual(resp, 0, "CL SIRTA bug message type")
+
+    def test_corrupted_mf(self):
+
+        date = "20200721"
+        test_ifile = self.IN_DIR + "07157_A202007210103_CL31-Roissy.dat"
+        test_ofile = TEST_OUT_DIR + "test_cl-sirta_20200721.nc"
+
+        resp = subprocess.check_call(
+            [
+                MAIN_DIR + PRGM,
+                date,
+                self.conf_file,
+                test_ifile,
+                test_ofile,
+                "-log_level",
+                "debug",
+                "-v",
+                "debug",
+            ]
+        )
+
+        self.assertEqual(resp, 0, "CL SIRTA corrupted message")
 
 
 if __name__ == "__main__":
