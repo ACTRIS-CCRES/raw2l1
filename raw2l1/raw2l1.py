1876f2330fa29515f4bd33b46a9fcaea04de1f41
Marc-Antoine Drouin <marc-antoine.drouin@lmd.ipsl.fr>
2022-09-19 10:29:09 +0200

    update raw2l1 version to 2.1.5b

--- raw2l1/raw2l1.py
+++ raw2l1/raw2l1.py
@@ -13,7 +13,7 @@ from tools.check_conf import check_conf
 from tools import create_netcdf as cnc
 
 __author__ = "Marc-Antoine Drouin"
-__version__ = "3.1.4"
+__version__ = "3.1.5b"
 
 NAME = "raw2l1"
 
