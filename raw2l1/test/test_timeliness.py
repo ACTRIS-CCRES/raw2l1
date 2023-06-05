cc4cbf6f33f93984afcfc271533dacd557d72678
Marc-Antoine Drouin <marc-antoine.drouin@lmd.ipsl.fr>
2023-06-05 10:40:17 +0200

    remove nose from dependencies

--- raw2l1/test/test_timeliness.py
+++ raw2l1/test/test_timeliness.py
@@ -2,31 +2,29 @@
 
 import os
 import subprocess
+import pytest
 import unittest
 
-from nose.tools import raises
-
 MAIN_DIR = os.path.dirname(os.path.dirname(__file__)) + os.sep
-CONF_DIR = MAIN_DIR + "conf" + os.sep
-TEST_DIR = MAIN_DIR + "test" + os.sep
-TEST_IN_DIR = TEST_DIR + os.sep + "input" + os.sep
-TEST_OUT_DIR = TEST_DIR + os.sep + "output" + os.sep
+TEST_DIR = os.path.join(MAIN_DIR, "test")
+CONF_DIR = os.path.join(TEST_DIR, "conf")
+TEST_IN_DIR = os.path.join(TEST_DIR, "input")
+TEST_OUT_DIR = os.path.join(TEST_DIR, "output")
 PRGM = "raw2l1.py"
 
 
 class TestTimeliness(unittest.TestCase):
-    IN_DIR = TEST_IN_DIR + "campbell_cs135" + os.sep
-    conf_file = CONF_DIR + "conf_campbell_cs135_toprof.ini"
+    IN_DIR = os.path.join(TEST_IN_DIR, "campbell_cs135")
+    conf_file = os.path.join(CONF_DIR, "conf_campbell_cs135_eprofile.ini")
 
-    @raises(subprocess.CalledProcessError)
     def test_too_old_auto(self):
         date = "20141030"
-        test_ifile = self.IN_DIR + "cs135-20150213-message006.txt"
-        test_ofile = TEST_OUT_DIR + "test_cs135_20150213_000000.nc"
+        test_ifile = os.path.join(self.IN_DIR, "cs135-20150213-message006.txt")
+        test_ofile = os.path.join(TEST_OUT_DIR, "test_cs135_20150213_000000.nc")
 
-        subprocess.check_call(
+        resp = subprocess.call(
             [
-                MAIN_DIR + PRGM,
+                os.path.join(MAIN_DIR, PRGM),
                 date,
                 self.conf_file,
                 test_ifile,
@@ -37,15 +35,16 @@ class TestTimeliness(unittest.TestCase):
             ]
         )
 
-    @raises(subprocess.CalledProcessError)
+        self.assertEqual(resp, 1, "timeliness too old auto")
+
     def test_too_old_duration_set(self):
         date = "20141030"
-        test_ifile = self.IN_DIR + "cs135-20150213-message006.txt"
-        test_ofile = TEST_OUT_DIR + "test_cs135_20150213_000000.nc"
+        test_ifile = os.path.join(self.IN_DIR, "cs135-20150213-message006.txt")
+        test_ofile = os.path.join(TEST_OUT_DIR, "test_cs135_20150213_000000.nc")
 
-        subprocess.check_call(
+        resp = subprocess.call(
             [
-                MAIN_DIR + PRGM,
+                os.path.join(MAIN_DIR, PRGM),
                 date,
                 self.conf_file,
                 test_ifile,
@@ -57,6 +56,8 @@ class TestTimeliness(unittest.TestCase):
                 "24",
             ]
         )
+
+        self.assertEqual(resp, 1, "timeliness too old duration set")
 
 
 if __name__ == "__main__":
