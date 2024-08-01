e02036bb421e0417f2624378385e6fb91ff056c2
Marc-Antoine Drouin <marc-antoine.drouin@lmd.ipsl.fr>
2024-08-01 15:32:42 +0200

    doc: fix style for function comment

--- tests/test_arg_parser.py
+++ tests/test_arg_parser.py
@@ -10,33 +10,27 @@ import raw2l1.tools.arg_parser as ag
 
 
 def test_args_date_impossible():
-    """
-    Test ArgumentTypeError is raised for non-existing date.
-    """
+    """Test ArgumentTypeError is raised for non-existing date."""
     with pytest.raises(argparse.ArgumentTypeError):
         ag.check_date_format("20150100")
 
 
 def test_args_date_bad_format():
-    """
-    Test ArgumentTypeError is raised for unrecognized format.
-    """
+    """Test ArgumentTypeError is raised for unrecognized format."""
     with pytest.raises(argparse.ArgumentTypeError):
         ag.check_date_format("2015-01-01")
 
 
-@pytest.mark.parametrize("date_str, date_dt", [("20150301", dt.datetime(2015, 3, 1))])
+@pytest.mark.parametrize(
+    ("date_str", "date_dt"), [("20150301", dt.datetime(2015, 3, 1))]
+)
 def test_args_date_parse(date_str, date_dt):
-    """
-    Test parsing of dates.
-    """
+    """Test parsing of dates."""
     assert ag.check_date_format(date_str) == date_dt
 
 
 def test_args_parsing(conf_dir, instr_dir_hatpro, output_dir):
-    """
-    Check full parsing of arguments.
-    """
+    """Check full parsing of arguments."""
     args = [
         "20160101",
         str(conf_dir / "conf_dummy.ini"),
@@ -48,7 +42,7 @@ def test_args_parsing(conf_dir, instr_dir_hatpro, output_dir):
 
     expected_args = {
         "date": dt.datetime(2016, 1, 1),
-        "conf": open(conf_dir / "conf_dummy.ini"),
+        "conf": (conf_dir / "conf_dummy.ini").open(),
         "input": [
             str(instr_dir_hatpro / "hatpro_0a_z1Imwrad-TPB_v01_20150901_000412_712.nc"),
             str(instr_dir_hatpro / "hatpro_0a_z1Imwrad-TPB_v01_20150901_120108_716.nc"),
